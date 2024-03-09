f490ed3418d913a9c5c67b87c24ee01859693dc1
YourAyami <margosudorzhenko@gmail.com>
2024-03-09 17:35:02 +0200

    Calkulator

---
+++ Lesson_2.py
@@ -0,0 +1,10 @@
+a = int(input('Введите первое число: '))
+b = int(input('Введите второе число: '))
+summ = a + b
+print(f'Сумма двух чисел: {summ}')
+sub = a - b
+print(f'Різниця двух чисел: {sub}')
+mult = a * b
+print(f'Добуток двух чисел: {mult}')
+div = a / b
+print(f'Частка двух чисел: {div}')
